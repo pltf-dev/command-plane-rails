94554ec05ff19654525cc9c4c5f7190f7b8029d2
yuce <yuce@commandplane.io>
2024-02-06 18:54:08 +0000

    ..

--- plane-app/config/puma.rb
+++ plane-app/config/puma.rb
@@ -5,7 +5,7 @@
 
 
 # YS added this for network access
-bind "tcp://0.0.0.0:3000"
+# bind "tcp://0.0.0.0:3000"
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -27,7 +27,7 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 
 # Specifies the `environment` that Puma will run in.
